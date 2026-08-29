38b642a9be6abe8c0aef4e594a2ddc9426cf1c52
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2026-08-28 22:11:53 -0300

    laço if dentro do if

---
+++ teste1.py
@@ -0,0 +1,5 @@
+def classificar_idade(idade):
+    if idade > 18:
+        return "Maior de Idade"
+    return "Menor de idade"
+print(classificar_idade(20))
---
+++ teste2.py
@@ -0,0 +1,3 @@
+from teste1 import classificar_idade
+
+print(classificar_idade(17))
